960e222804cde726a066fc7935bb4e8bdb9b3044
Wallace Da Silva Ferreira <wallace.ferreira@pagar.me>
2023-06-02 21:46:37 -0400

    :white_check_mark: creates unit tests for VetClinicService

---
+++ src/tests/VetClinicService.spec.ts
@@ -0,0 +1,28 @@
+﻿import 'mocha';
+import {expect} from "chai";
+
+import {VetClinicService} from "../services/VetClinicService";
+import {IClinic} from "../models/IClinic";
+
+describe('VetClinicService', () => {
+    it('Test return vet clinic client', () => {
+        let vetClinicService = new VetClinicService();
+        const vetClinic = vetClinicService.getClinic();
+
+        return vetClinic.then((clinicList: IClinic[]) => {
+            clinicList.forEach((clinic: IClinic) => {
+                expect(clinic.name).to.be.a('string');
+                expect(clinic.state).to.be.a('string');
+                expect(clinic.availability.from).to.be.a('string');
+                expect(clinic.availability.to).to.be.a('string');
+            })
+        })
+    });
+
+    it('Test check is instance of Promise', () => {
+        let vetClinicService = new VetClinicService();
+        const vetClinic = vetClinicService.getClinic();
+
+        expect(vetClinic instanceof Promise).to.equal(true);
+    });
+});
